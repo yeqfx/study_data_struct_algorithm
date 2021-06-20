13dfdee2ae70503bb1a30bd735248cf824e0dab4
yeqfx <yeqfx@hotmail.com>
2021-06-20 14:30:34 +0000

    4.3. Check Brackets

--- ds_easily_written_in_python/python/Chap04/Sec03_Check_Brackets/CheckBrackets.py
+++ ds_easily_written_in_python/python/Chap04/Sec03_Check_Brackets/CheckBrackets.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 # from utilities import checkBrackets
-from utilities import checkBrackets
+from myutil import checkBrackets
 
 if __name__ == "__main__":
     
